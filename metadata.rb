bbe5422ed09d445cfcb690c1ac6c80d2cb85ee5d
Daniel Jabbour <daniel@amoe.ba>
2014-01-07 22:15:37 -0800

    new version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "sayhi@amoe.ba"
 license          "MIT"
 description      "Deploy a web application (Ruby on Rails or NodeJS) using industry best practices. Used with Amoeba Deploy Tools."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.1.1"
 
 recipe           "webapp", "Default webapp recipe"
 
