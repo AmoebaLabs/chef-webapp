62e8c11da7133c13c147d89cfe856f4438552aca
radilr <radil@fandrop.com>
2014-04-08 18:39:27 +0300

    support non bundle cron jobs

--- recipes/cron.rb
+++ recipes/cron.rb
@@ -5,7 +5,12 @@ app.crontab.each do |c|
     if c[:action] && c[:action].to_sym == :delete
       action :delete
     else
-      command "bash -c 'source #{app.user.home}/.rvm/scripts/rvm; cd #{app.current_path}; bundle exec #{c.command} >> #{app.log_path}/cron.log 2>&1'"
+      if (!c.include?(:include_bundle) || c.include_bundle == true)
+        command = "bundle exec #{c.command}"
+      else
+        command = c.command
+      end
+      command "bash -c 'source #{app.user.home}/.rvm/scripts/rvm; cd #{app.current_path}; #{command} >> #{app.log_path}/cron.log 2>&1'"
 
       environment app.env_vars
 
