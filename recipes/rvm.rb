33a2a67ca130a26c9c2bc155b32aa5f31b5ef7b5
radilr <radil@fandrop.com>
2014-04-11 15:06:53 +0300

    cleanup ensure line WARN

--- recipes/rvm.rb
+++ recipes/rvm.rb
@@ -23,8 +23,8 @@ include_recipe 'rvm::user'
 
 rvm_script = '$HOME/.rvm/scripts/rvm'
 ensure_line "#{app.user.home}/.bashrc" do
-  content %{[[ -s "#{rvm_script}" ]] && source "#{rvm_script}"}
-end
-ensure_line "#{app.user.home}/.bashrc" do
-  content %{export PATH=$PATH:$HOME/.rvm/bin}
+  content [
+              %{[[ -s "#{rvm_script}" ]] && source "#{rvm_script}"},
+              %{export PATH=$PATH:$HOME/.rvm/bin}
+          ]
 end
